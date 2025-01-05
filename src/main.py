c7a643657dad79981d1aa5cfb93095906b392c8f
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-01-05 15:22:04 +0300

    First homework

---
+++ src/main.py
@@ -0,0 +1,34 @@
+from func import Category, Product
+import json
+
+dict_of_categoryes = {}
+dict_of_products = {}
+
+with open('products.json', encoding='utf-8') as file:
+    data = json.load(file)
+for i in range(len(data)):
+    name = data[i]['name']
+    desc = data[i]['description']
+    products = data[i]['products']
+    dict_of_categoryes[i] = Category(name, desc, products)
+
+count = 0
+for i in range(len(data)):
+    for n in range(len(data[i]['products'])):
+        name = data[i]['products'][n]['name']
+        desc = data[i]['products'][n]['description']
+        price = data[i]['products'][n]['price']
+        quantity = data[i]['products'][n]['quantity']
+        dict_of_products[count] = Product(name, desc, price, quantity)
+        count += 1
+
+print(dict_of_products)
+print(dict_of_products[0].name)
+print(dict_of_products[1].price)
+print(dict_of_products[2].desc)
+print(dict_of_products[3].quantity)
+print(dict_of_categoryes)
+print(dict_of_categoryes[0].name)
+print(dict_of_categoryes[1].desc)
+print(dict_of_categoryes[1].products)
+print(dict_of_categoryes[0].uniq_products)
