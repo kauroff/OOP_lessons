e515cb2d932085c950fc34d78a1d32000314b238
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-01-09 14:43:23 +0300

    Fourth homework

--- src/mixin.py
+++ src/mixin.py
@@ -2,9 +2,11 @@ class MixinRepr:
     """
     Микси-класс, который выводит информацию в консоль о том, что был создан объект
     """
+
     def __repr__(self):
         """
         Дандер-метод для вывода отладочной информации
         :return: отладочная инфа
         """
-        return f'{type(self)}({vars(self)})'
+        data = [unit for unit in vars(self).values()]
+        return f'{type(self).__name__}({data})'
